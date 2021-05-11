b47c782654bdf1633046cf520a6b93611411127a
Aloxan12 <kvakvaaak@mail.ru>
2021-05-11 14:11:33 +0300

    update and 5homework

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import {PATH} from "./Routes";
+import s from "./Header.module.css";
 
 function Header() {
     return (
         <div>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.link}></NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.link}></NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.link}></NavLink>
             // add NavLinks
 
         </div>
