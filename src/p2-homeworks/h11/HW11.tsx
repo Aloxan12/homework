6387e29d9eef96c24a96c03ef23c60203b2bd4ed
Aloxan12 <kvakvaaak@mail.ru>
2021-06-04 11:09:25 +0300

    homework 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,6 +6,12 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const setValue = (value: [number, number]) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
+
+
     return (
         <div>
             <hr/>
@@ -14,16 +20,13 @@ function HW11() {
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+                <SuperRange value={value1} onChangeRange={setValue1}/>
             </div>
 
             <div>
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
+                <SuperDoubleRange value={[value1, value2]} onChangeRange={setValue} />
+
                 <span>{value2}</span>
             </div>
 
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
+import {Slider} from "@material-ui/core";
+import s from '../c7-SuperRange/SuperRange.module.css'
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
     value?: [number, number]
-    // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+    const handleChange = (event: any, newValue: number | number[]) => {
+
+        onChangeRange && onChangeRange(newValue as [number, number]);
+    };
 
     return (
-        <>
-            DoubleRange
-        </>
+        <span className={s.range}>
+            <Slider
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider"
+                step={5}
+            />
+        </span>
     )
 }
 
