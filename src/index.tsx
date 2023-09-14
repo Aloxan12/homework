a599c6595370c055cfad4753436f4109efc1fb31
Aloxan12 <kvakvaaak@mail.ru>
2023-09-14 10:59:32 +0300

    homework 5

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './s1-main/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
