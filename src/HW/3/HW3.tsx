a4a2b4c3d327baff1c2311c2142a6d993efc92dd
Aloxan12 <kvakvaaak@mail.ru>
2023-09-14 10:39:13 +0300

    homework 4

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -20,7 +20,7 @@ export const HW3 = () => {
   };
 
   const handleSave = () => {
-    setTexts(prevState => [...prevState, currentText])
+    setTexts(prevState => [currentText, ...prevState])
     setCurrentText('')
   };
 
@@ -29,7 +29,7 @@ export const HW3 = () => {
       {currentText ? (
         <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
-        <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
+        <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1>
       )}
 
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
